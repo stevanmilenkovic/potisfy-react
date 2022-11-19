9b80249b9947988df1415da1584f1c4f05ee1bff
Čika Đole <djordje.simic1311@gmail.com>
2022-11-19 18:53:14 +0100

    removed auth token

--- src/Misc.js
+++ src/Misc.js
@@ -1,4 +1,4 @@
-const oAuthToken = 'BQAxFa7OIewpQ5hE6mjLHtSISimJ7xAu_FtCGKxbTax9uP3udJ4pAtPe9sC4XuKixl1ye0Ge-4jxYUsmYlr_6pJYcFbvO0d6OqZYoH5j3Fx-JwRMyxBDiRokfx8zwr0zWXATl2hs0ZFxFZT8h1i4EayQgcNJI-TJZNriwAEB4SfjFdbhdQGGQWRUilsr0z1c6g8';
+const oAuthToken = '';
 // get token at https://developer.spotify.com/console/get-search-item/
 
 export const createNumberString = (num) => {
